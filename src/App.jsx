bd73c95af09aca6b3ee9f2fd7d7db55bb9b667c7
Bikram <technology2203@gmail.com>
2025-03-24 03:05:09 +0530

    Avoid Intersecting states

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,12 @@ import Player from "./components/Player";
 import GameBoard from "./components/GameBoard";
 
 function App() {
+  const [gameTurns, setGameTurns] = useState([])
   const [activePlayer, setActivePlayer] = useState("X");
 
   function handleSelectSquare() {
     setActivePlayer((curActivePlayer) => (curActivePlayer === "X" ? "O" : "X"));
+    setGameTurns();
   }
 
   return (
